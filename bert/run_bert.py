359a6cd8f9a149125a3c39e7dfade7f0d52a86c3
purohit10saurabh <purohit10saurabh@gmail.com>
2020-05-30 13:50:33 +0530

    meesage

---
+++ bert/run_bert.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import subprocess
+import pdb
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def runBashCommand(command, args):
+	bash_command = bash_command = "/usr/bin/time -f '\n" + bcolors.OKBLUE + "Total Time : %E (mm:ss)\nMax RAM : %M kB" + bcolors.ENDC + "\n' "
+	display_command = command
+
+	for arg in args:
+		display_command += " " + arg
+	bash_command += display_command
+
+	print("Running command : " + display_command)
+	
+	if(os.system(bash_command) == 0):
+		print(bcolors.OKGREEN + "Successfully" + bcolors.ENDC + " ran bash command : " + display_command)
+	else:
+		print(bcolors.FAIL + "Failure" + bcolors.ENDC + " while running bash command : " + display_command)
+		exit(0)
+
+path="/workspace/data/bert/"
+BERT_BASE_DIR=path+"uncased_L-12_H-768_A-12/"
+
+input_file=sys.argv[1]
+output_file=sys.argv[2]
+#data = '/mnt/t-sapuro/XC/data/new-eurlex/'
+#input_file = data + 'tst_X.content_hr.txt'
+#output_file= data + 'tst_X.content_hr_bert.txt'
+#input_file = data + 'Y.title.txt'
+#output_file= data + 'Y.title_bert.txt'
+#input_file = data + 'my_Y.txt'
+#output_file= data + 'my_Y_bert.txt'
+vocab_file=BERT_BASE_DIR+"vocab.txt"
+bert_config_file=BERT_BASE_DIR+"bert_config.json"
+init_checkpoint=BERT_BASE_DIR+"bert_model.ckpt"
+
+if __name__ == "__main__":
+	bert_args = [ 	"--input_file", input_file,
+					"--output_file", output_file,
+					"--vocab_file", vocab_file,
+					"--bert_config_file", bert_config_file,	
+					"--init_checkpoint", init_checkpoint,
+					"--layers", '-1',	
+					"--max_seq_length", str(256),
+					"--batch_size", str(8),
+				]
+
+	for i in range(len(bert_args)):
+		arg = bert_args[i]
+		#if ((arg[0] == '-') and (arg in sys.argv)):
+		#	bert_args[i+1] = str(sys.argv[sys.argv.index(arg) + 1])
+
+	bert_command = "python ./bert/extract_features.py"
+	runBashCommand(bert_command, bert_args)
+	
