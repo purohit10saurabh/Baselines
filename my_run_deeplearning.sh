7b5cc40b510874f2d427f6c6823746638c17e26b
root <root@T-SAPURO-SP4.fareast.corp.microsoft.com>
2020-08-03 12:09:08 +0530

    philly

--- my_run_deeplearning.sh
+++ my_run_deeplearning.sh
@@ -18,7 +18,7 @@ convert () {
 }
 
 #work_dir="/mnt/t-sapuro/XC"
-work_dir="/workspace/"
+work_dir="/mnt/output/"
 data_dir="${work_dir}/data/${dataset}"
 model_data_dir="${work_dir}/data/${data_version}/${dataset}"
 mkdir -p "${model_data_dir}"
